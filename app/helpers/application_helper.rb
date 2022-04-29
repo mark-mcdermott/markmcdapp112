d5200522aa3844f72e40cf89d094bd73e4dff5c1
Mark McDermott <mark@markmcdermott.io>
2022-04-28 21:50:49 -0500

    active links on nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def nav_link(text, path, page) 
+    active_class = text == page ? 'active' : ''
+    link_to text, path, class: "nav-link " + active_class
+  end
+  
 end
