23dd76de1a8e963bf7305fb1014556fff069eb82
Mark McDermott <mark@markmcdermott.io>
2022-04-29 00:22:14 -0500

    contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :users
+  resources :contacts, only: [:new, :create]
   
   root 'home#index'
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
+  get '/contact', to: 'contacts#new'
   
 
 end
